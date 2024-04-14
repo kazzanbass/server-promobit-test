a1396a1f990856cda20613e6703009dbf20f53c8
Roman Kornienko <117166553+kazzanbass@users.noreply.github.com>
2024-04-15 04:21:32 +0600

    Update client.cpp
    Добавил комментарии client.cpp

--- client-server/client.cpp
+++ client-server/client.cpp
@@ -9,13 +9,13 @@
 
 int main(){
 	
-	int fd = Socket(AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in adr = {0};
-	adr.sin_family = AF_INET;
-	adr.sin_port = htons(34543);
-	Inet_pton(AF_INET, "127.0.0.1", &adr.sin_addr);
-	Connect(fd, (struct sockaddr *)&adr, sizeof adr);
-	write(fd, "Hello\n", 6);
+	int fd = Socket(AF_INET, SOCK_STREAM, 0); //Присваиваем дескриптор сокета fd
+	struct sockaddr_in adr = {0};//инициализируем структуру соккета 0
+	adr.sin_family = AF_INET;//Семейство протоколов IPv4
+	adr.sin_port = htons(34543);//Порт 34543
+	Inet_pton(AF_INET, "127.0.0.1", &adr.sin_addr);//Присваиваем ip адрес нашего компьютера структуре
+	Connect(fd, (struct sockaddr *)&adr, sizeof adr);//Подключаемся к сокету(структуре) сервера
+	write(fd, "Hello\n", 6); //отправляем сообщение
 	char buf[256];
 	ssize_t nread;
 	nread = read(fd, buf, 256);
