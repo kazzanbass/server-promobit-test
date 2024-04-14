402c22c963dbb2cb2dea270c5f189da736b7da82
Roman Kornienko <117166553+kazzanbass@users.noreply.github.com>
2024-04-15 04:22:32 +0600

    Update server.cpp
    Добавил комментарии server

--- client-server/server.cpp
+++ client-server/server.cpp
@@ -7,17 +7,17 @@
 
 int main(){
 	write(STDOUT_FILENO, "Сервер запущен\n", 29);
-	int server = Socket(AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in adr = {0};
-	adr.sin_family = AF_INET;
-	adr.sin_port = htons(34543);
-	Bind(server, (struct sockaddr *)&adr, sizeof adr);
-	Listen(server, 5);
-	socklen_t adrlen = sizeof adr;
-	int fd = Accept(server, (struct sockaddr *)&adr, &adrlen);
-	ssize_t nread;
+	int server = Socket(AF_INET, SOCK_STREAM, 0);//присваиваем дескриптор сокета server
+	struct sockaddr_in adr = {0};//инициализируем структуру данных
+	adr.sin_family = AF_INET; //семейство протоколов IPv4
+	adr.sin_port = htons(34543);//присваиваем сокету порт 34543
+	Bind(server, (struct sockaddr *)&adr, sizeof adr);//привязываем сокет к структуре (порт)
+	Listen(server, 5);//Прослушиваем порт до 5 клиентов
+	socklen_t adrlen = sizeof adr;//инициализируем адрес структуры
+	int fd = Accept(server, (struct sockaddr *)&adr, &adrlen);//присваиваем дескриптор сокета клиента
+	ssize_t nread;//размер посланного клиентом сообщения
 	char buf[256];
-	nread = read(fd, buf, 256);
+	nread = read(fd, buf, 256);//записываем сообщение клиента в буфер
 	if(nread==-1){
 		perror("read error");
 		exit(1);
@@ -27,8 +27,8 @@ int main(){
 	}
 	write(STDOUT_FILENO, buf, nread);
 	write(fd, buf, nread);
-	close(fd);
-	close(server);
+	close(fd);//Закрываем сокет клиента
+	close(server); //Закрываем сокет сервера
 	
 	return 0;
 }
