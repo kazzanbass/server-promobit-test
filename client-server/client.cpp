de4e73991fa5053dd277d2124d9f80129ee74698
Roman Kornienko <117166553+kazzanbass@users.noreply.github.com>
2024-04-18 06:06:12 +0600

    убрал интерфейс

--- client-server/client.cpp
+++ client-server/client.cpp
@@ -10,9 +10,9 @@
 using namespace std;
 
 void send_message(int fd){
-	char message[128];
-	cout<<"Введите сообщение(128 байт) ==> ";
-	cin>>message;
+	char *message = "Hello!";
+	//cout<<"Введите сообщение(128 байт) ==> ";
+	//cin>>message;
 	write(fd, message, 128);
 }
 
@@ -24,21 +24,12 @@ int main(){
         adr.sin_port = htons(8080);//Порт 8080
         Inet_pton(AF_INET, "127.0.0.1", &adr.sin_addr);//Присваиваем ip адрес нашего компьютера структуре
         Connect(fd, (struct sockaddr *)&adr, sizeof adr);//Подключаемся к серверу    
-        char answ = ' ';  
-        while(true){
-		cout<<"Отправить сообщение?(y/n) ==> ";
-		cin>>answ;
-		if(answ=='n')
-			break;
 		send_message(fd); //Отправляем сообщение серверу
 		char buf[128];
 		read(fd, buf, 128);//Читаем сообщение от сервера
-		cout<<buf<<endl;//Выводим ответ на консоль
-	}
-		
-	write(fd, "\END", 4);//Отправляем серверу сигнал о прекращении передачи сообщений
-			
-        close(fd);//Закрываем сокет
+		close(fd);//Закрываем сокет
+		cout<<buf<<endl;//Выводим ответ на консоль			
+        
         
         return 0;
 }
